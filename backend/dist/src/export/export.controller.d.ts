b091617cd6d3e5f0fe9ef70e3508d232932459b8
Fabian Garcia <fa.garciaa92@gmail.com>
2026-05-29 10:40:09 -0600

    Remove dist from repo, use postinstall build

--- backend/dist/src/export/export.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { ExportService } from './export.service';
-export declare class ExportController {
-    private svc;
-    constructor(svc: ExportService);
-    exportRanking(tid: string, fmt: string, res: Response): Promise<void>;
-}
--- backend/dist/src/export/export.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class ExportService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    exportRankingExcel(tournamentId: string): Promise<Buffer>;
-    exportRankingCsv(tournamentId: string): Promise<string>;
-}
